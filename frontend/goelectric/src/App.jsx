11a1d1f24e452ef7ef1d9ce97c9282ecadd12d02
Jeremi Sobierski <tbuli1112@gmail.com>
2022-04-08 16:48:54 +0200

    adds mui setup

--- frontend/goelectric/src/App.jsx
+++ frontend/goelectric/src/App.jsx
@@ -1,44 +1,31 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#3f51b5',
+      light: '#757de8',
+      dark: '#002984',
+      contrastText: '#ffffff'
+    },
+    secondary: {
+      main: '#26c6da',
+      light: '#6ff9ff',
+      dark: '#0095a8',
+      contrastText: '#000000'
+    }
+  },
+  spacing: 5,
+});
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <div>
+        dome bullshit
+      </div>
+    </ThemeProvider>
   )
 }
 
--- frontend/goelectric/src/main.jsx
+++ frontend/goelectric/src/main.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
+import * as ReactDOMClient from 'react-dom/client'
 import App from './App'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
-ReactDOM.render(
+ReactDOMClient.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App/>}>
+          <Route index element={<div>Dupa</div>}/>
+          <Route path='dupa' element={<div>dupa</div>}/>
+          <Route path='login' element={<LoginForm/>}/>
+          <Route path='dishes' element={<WithUser>{user => <Dishes user={user}/>}</WithUser>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
